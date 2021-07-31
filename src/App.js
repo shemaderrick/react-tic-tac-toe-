a5cc7b7166847169ae34bb2d80d679a8787d0e12
shemaderrick <shemakderrick@gmail.com>
2021-07-31 18:55:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from 'react'
+import './App.css'
+import Game from './components/game'
+import calculateWinner from './helpers/winner'
+export default function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [xisNext, setXisNext] = useState(true)
+  const [counter, setCounter] = useState(0)
+  const [nextPlayer, setNextPlayer] = useState('Next player is X')
+  const winner = calculateWinner(board)
+  console.log(winner)
+  const handlePlayer = (index) => {
+    setNextPlayer(!xisNext ? 'Next player: X' : "Next player: 0")
+    let counterCopy = counter + 1
+    setCounter(counterCopy)
+    const boardCopy = [...board]
+    if (winner || boardCopy[index]) {
+      return
+    }
+    boardCopy[index] = xisNext ? 'X' : '0'
+    setBoard(boardCopy)
+    setXisNext(!xisNext)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Game handlePlayer={handlePlayer} board={board} winner={winner} counter={counter} nextPlayer={nextPlayer} setBoard={setBoard} setCounter={setCounter} />
+    </>)
 }
 
-export default App;
---
+++ src/components/game.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Game = ({ handlePlayer, board, winner, counter, nextPlayer, setBoard, setCounter }) => {
+ return (<>
+  <div className="grid-container">
+   {board.map((item, index) => {
+    return <button key={index} className="square" id={item}
+     value={item}
+     onClick={(e) => handlePlayer(index)}
+    >
+     {item}
+    </button>
+   })}
+  </div>
+  <div className="status">
+   <p>{winner ? 'Winner: ' + winner : counter === 9 ? "Tie" : ""}</p>
+   <p>{counter === 9 ? '' : winner ? '' : nextPlayer}</p>
+   {winner || counter === 9 ? <button className='reset' onClick={() => { setBoard(Array(9).fill(null)); setCounter(0) }}>Reset</button> : ''}
+  </div>
+ </>)
+
+
+}
+
+export default Game
